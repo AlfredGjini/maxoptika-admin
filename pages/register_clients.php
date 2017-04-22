8b33901b7680da35f42743b9bf6ddb59e484b622
USER1-PC\user <a.gjini@live.com>
2017-04-22 16:14:16 +0200

    tes

--- pages/register_clients.php
+++ pages/register_clients.php
@@ -127,7 +127,7 @@
                   'Perdoruesi u regjistrua me sukses',
                   'success'
                 )
-                $('#register_clients').trigger("reset");               
+                //$('#register_clients').trigger("reset");               
 
               },
               error: function(jqXHR, textStatus, errorThrown) {
