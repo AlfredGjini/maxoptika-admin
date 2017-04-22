62ea4a543c270ff45c4f63324c5ba1f165dfec1a
AlfredGjini <a.gjini@live.com>
2017-04-22 10:36:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+    // $connection = pg_connect ("host=ec2-23-21-55-25.compute-1.amazonaws.com dbname=d3fn4lugik4eop user=dekixmpqcprkpu password=MNbCC56WQ1ZaNRqX8GHmTBaUv-");
+    // if($connection) {
+    //    echo 'connected';
+    // } else {
+    //     echo 'there has been an error connecting';
+    // } 
+
+
+// $dbuser = 'dekixmpqcprkpu';  
+// $dbpass = 'MNbCC56WQ1ZaNRqX8GHmTBaUv-';  
+// $host = 'ec2-23-21-55-25.compute-1.amazonaws.com';  
+// $dbname='d3fn4lugik4eop';  
+// $dbh = new PDO("pgsql:host=$host;dbname=$dbname;sslmode=require", $dbuser, $dbpass);  
+echo "hello world";
+
+
+    //phpinfo();
+?>
