938714aba48e70944196894087374b8c1a0e1897
AlfredGjini <a.gjini@live.com>
2017-04-22 10:42:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,11 @@ $dbuser = 'dekixmpqcprkpu';
 $dbpass = 'MNbCC56WQ1ZaNRqX8GHmTBaUv-';  
 $host = 'ec2-23-21-55-25.compute-1.amazonaws.com';  
 $dbname='d3fn4lugik4eop';  
-$dbh = new PDO("pgsql:host=$host;dbname=$dbname;sslmode=require", $dbuser, $dbpass);  
+$dbh = new PDO("pgsql:host=$host;dbname=$dbname;sslmode=require", $dbuser, $dbpass);
+}catch (PDOException $e){  
+ echo "Error!: " . $e->getMessage() . "<br/>";  
+ die();  
+ }
 echo "hello world";
 
 
