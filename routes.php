2599e035bd1b60969c54fb66989e70343001a9ff
USER1-PC\user <a.gjini@live.com>
2017-04-24 13:54:35 +0200

    Added the new routes

--- routes.php
+++ routes.php
@@ -6,8 +6,23 @@ switch ($page) {
 		include "pages/register_clients.php";
 		break;
 	
+	case 'dashboard':
+		include "pages/dashboard.php";
+		break;
 	case 'manage_clients':
 		include "pages/manage_clients.php";
+		break;
+	case 'manage_reservations':
+		include "pages/manage_reservations.php";
+		break;
+
+	case 'create_clinic_card':
+		include "pages/create_clinic_card.php";
+		break;	
+
+		
+	case 'manage_clinic_card':
+		include 'pages/manage_clinic_card.php';	
 		break;
 	default:
 		include "pages/dashboard.php";
