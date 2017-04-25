d5152910e84cf3c25e5af6bc182197e584684519
USER1-PC\user <a.gjini@live.com>
2017-04-25 12:48:56 +0200

    dfgdg

--- pages/create_clinic_card.php
+++ pages/create_clinic_card.php
@@ -41,7 +41,7 @@
 		                    <h4>Data e vizites :</h4>
 	                    	<div class="form-group">
 			                    <div class='input-group date' id='datetimepicker1'>
-				                    <input id="datazgjedhur" name="date" type='text' class="form-control" />
+				                    <input id="datazgjedhur" name="date" type='text' class="form-control" value="" />
 				                    <span class="input-group-addon">
 				                        <span class="glyphicon glyphicon-calendar"></span>
 				                    </span>
