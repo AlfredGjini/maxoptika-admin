d06c42a0838897ca74aba18c10cac878a3735f35
USER1-PC\user <a.gjini@live.com>
2017-04-27 14:08:09 +0200

    sdsvdf

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,91 +2,6 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard
-        <small>Control panel</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-      <!-- Small boxes (Stat box) -->
-      <div class="row">
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>User Registrations</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <?php if (isset($_SESSION['username']) && $_SESSION['username']=="superadmin" ) { ?>
-        <div class="col-lg-3 col-xs-6 perditeso" style="cursor:pointer;">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h2 style="font-weight: bold;">Perditeso te dhenat</h2>
-
-              <!-- <p>Unique Visitors</p> -->
-            </div>
-            <div class="icon" style="font-size: 40px;top: 25px;">
-              <i class="fa fa-database"></i>
-            </div>
-            <!-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
-          </div>
-        </div>
-        <!-- ./col -->
-        <?php } ?>
-      </div>
-      <!-- /.row -->
-      <!-- Main row -->
-      
-    </section>
-    <!-- /.content -->
-  </div>
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
         Paneli I Administratorit
       </h1>
       <ol class="breadcrumb">
@@ -149,7 +64,7 @@
             <div class="inner">
               <h3 class="numberOfClinicCards"></h3>
 
-              <p>Kartela Klinike</p>  
+              <p>Kartela Klinike</p>
             </div>
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
