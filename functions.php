760a4bf97e15f4e9f640e2e213d85b13f4f9a3a1
USER1-PC\user <a.gjini@live.com>
2017-04-28 12:58:32 +0200

    uhkuyik

--- functions.php
+++ functions.php
@@ -246,30 +246,30 @@ switch ($action) {
 	case 'update_db_on_cron':
 		//$params = Input::get('param');
 		$gjendjet =  $data;
-		var_dump($gjendjet);
+		var_dump($data);
 		echo "kot update";
 		//$gjendje = $params['']
 		//$artikuj_no = count($artikujt);
 		//print_r($artikujt);
 		//print_r($artikujt) ; 
-		foreach ($gjendjet as $key => $artikull) {
-			// $product_exist = DB::getInstance()->get('cmime',[['idprodukti','=',$gjendje['KODARTIKULLI']]])->firstResult();
-			$product_exist = DB::getInstance()->get('products2',[['kodartikulli','=',$artikull['KODARTIKULLI']]])->firstResult();
+		// foreach ($gjendjet as $key => $artikull) {
+		// 	// $product_exist = DB::getInstance()->get('cmime',[['idprodukti','=',$gjendje['KODARTIKULLI']]])->firstResult();
+		// 	$product_exist = DB::getInstance()->get('products2',[['kodartikulli','=',$artikull['KODARTIKULLI']]])->firstResult();
 			
-			if($product_exist!=0){
+		// 	if($product_exist!=0){
 
 
-				$delete_product = DB::getInstance()->delete('products2',[['kodartikulli','=',$artikull['KODARTIKULLI']]]);
-				$update_products = DB::getInstance()->insert('products2',[strtolower('KODARTIKULLI')=>$artikull['KODARTIKULLI'],strtolower('PERSHKRIMARTIKULLI')=>$artikull['PERSHKRIMARTIKULLI'],strtolower('PERSHKRIMIANGARTIKULLI')=>$artikull['PERSHKRIMIANGARTIKULLI'],strtolower('KODNJESIA1')=>$artikull['KODNJESIA1'],strtolower('KODNJESIA2')=>$artikull['KODNJESIA2'],strtolower('KOEFICENTARTIKULLI')=>$artikull['KOEFICENTARTIKULLI'],strtolower('MAGAZINA')=>$artikull['MAGAZINA'],strtolower('AKTIV')=>$artikull['AKTIV'],strtolower('KLASA')=>$artikull['KLASA'],strtolower('GRUPI')=>$artikull['GRUPI'],strtolower('NENGRUPI')=>$artikull['NENGRUPI'],strtolower('TVSHKODI')=>$artikull['TVSHKODI'],strtolower('VENDODHJEARTIKULLI')=>$artikull['VENDODHJEARTIKULLI'],strtolower('PERSHKRIMI')=>$artikull['PERSHKRIMI'],strtolower('IMAZH')=>$artikull['IMAZH'],strtolower('VLERATVSH')=>$artikull['VLERATVSH'],strtolower('KODIDOGANORARTIKULLI')=>$artikull['KODIDOGANORARTIKULLI'],strtolower('ARTREF')=>$artikull['ARTREF'],strtolower('KODIFIKIMARTIKULLI')=>$artikull['KODIFIKIMARTIKULLI'],strtolower('KODIFIKIMARTIKULLI2')=>$artikull['KODIFIKIMARTIKULLI2'],strtolower('DTMODIFIKIM')=>$artikull['DTMODIFIKIM'],strtolower('IDSTATUSDOK')=>$artikull['IDSTATUSDOK'],strtolower('PERPESHORE')=>$artikull['PERPESHORE'],strtolower('LLOJGARANCIA')=>$artikull['LLOJGARANCIA'],strtolower('GARANCIA')=>'0']);
+		// 		$delete_product = DB::getInstance()->delete('products2',[['kodartikulli','=',$artikull['KODARTIKULLI']]]);
+		// 		$update_products = DB::getInstance()->insert('products2',[strtolower('KODARTIKULLI')=>$artikull['KODARTIKULLI'],strtolower('PERSHKRIMARTIKULLI')=>$artikull['PERSHKRIMARTIKULLI'],strtolower('PERSHKRIMIANGARTIKULLI')=>$artikull['PERSHKRIMIANGARTIKULLI'],strtolower('KODNJESIA1')=>$artikull['KODNJESIA1'],strtolower('KODNJESIA2')=>$artikull['KODNJESIA2'],strtolower('KOEFICENTARTIKULLI')=>$artikull['KOEFICENTARTIKULLI'],strtolower('MAGAZINA')=>$artikull['MAGAZINA'],strtolower('AKTIV')=>$artikull['AKTIV'],strtolower('KLASA')=>$artikull['KLASA'],strtolower('GRUPI')=>$artikull['GRUPI'],strtolower('NENGRUPI')=>$artikull['NENGRUPI'],strtolower('TVSHKODI')=>$artikull['TVSHKODI'],strtolower('VENDODHJEARTIKULLI')=>$artikull['VENDODHJEARTIKULLI'],strtolower('PERSHKRIMI')=>$artikull['PERSHKRIMI'],strtolower('IMAZH')=>$artikull['IMAZH'],strtolower('VLERATVSH')=>$artikull['VLERATVSH'],strtolower('KODIDOGANORARTIKULLI')=>$artikull['KODIDOGANORARTIKULLI'],strtolower('ARTREF')=>$artikull['ARTREF'],strtolower('KODIFIKIMARTIKULLI')=>$artikull['KODIFIKIMARTIKULLI'],strtolower('KODIFIKIMARTIKULLI2')=>$artikull['KODIFIKIMARTIKULLI2'],strtolower('DTMODIFIKIM')=>$artikull['DTMODIFIKIM'],strtolower('IDSTATUSDOK')=>$artikull['IDSTATUSDOK'],strtolower('PERPESHORE')=>$artikull['PERPESHORE'],strtolower('LLOJGARANCIA')=>$artikull['LLOJGARANCIA'],strtolower('GARANCIA')=>'0']);
 				
-			}
+		// 	}
 			
-			else{
+		// 	else{
 
-				$insert_into_products = DB::getInstance()->insert('products2',[strtolower('KODARTIKULLI')=>$artikull['KODARTIKULLI'],strtolower('PERSHKRIMARTIKULLI')=>$artikull['PERSHKRIMARTIKULLI'],strtolower('PERSHKRIMIANGARTIKULLI')=>$artikull['PERSHKRIMIANGARTIKULLI'],strtolower('KODNJESIA1')=>$artikull['KODNJESIA1'],strtolower('KODNJESIA2')=>$artikull['KODNJESIA2'],strtolower('KOEFICENTARTIKULLI')=>$artikull['KOEFICENTARTIKULLI'],strtolower('MAGAZINA')=>$artikull['MAGAZINA'],strtolower('AKTIV')=>$artikull['AKTIV'],strtolower('KLASA')=>$artikull['KLASA'],strtolower('GRUPI')=>$artikull['GRUPI'],strtolower('NENGRUPI')=>$artikull['NENGRUPI'],strtolower('TVSHKODI')=>$artikull['TVSHKODI'],strtolower('VENDODHJEARTIKULLI')=>$artikull['VENDODHJEARTIKULLI'],strtolower('PERSHKRIMI')=>$artikull['PERSHKRIMI'],strtolower('IMAZH')=>$artikull['IMAZH'],strtolower('VLERATVSH')=>$artikull['VLERATVSH'],strtolower('KODIDOGANORARTIKULLI')=>$artikull['KODIDOGANORARTIKULLI'],strtolower('ARTREF')=>$artikull['ARTREF'],strtolower('KODIFIKIMARTIKULLI')=>$artikull['KODIFIKIMARTIKULLI'],strtolower('KODIFIKIMARTIKULLI2')=>$artikull['KODIFIKIMARTIKULLI2'],strtolower('DTMODIFIKIM')=>$artikull['DTMODIFIKIM'],strtolower('IDSTATUSDOK')=>$artikull['IDSTATUSDOK'],strtolower('PERPESHORE')=>$artikull['PERPESHORE'],strtolower('LLOJGARANCIA')=>$artikull['LLOJGARANCIA'],strtolower('GARANCIA')=>'0']);
+		// 		$insert_into_products = DB::getInstance()->insert('products2',[strtolower('KODARTIKULLI')=>$artikull['KODARTIKULLI'],strtolower('PERSHKRIMARTIKULLI')=>$artikull['PERSHKRIMARTIKULLI'],strtolower('PERSHKRIMIANGARTIKULLI')=>$artikull['PERSHKRIMIANGARTIKULLI'],strtolower('KODNJESIA1')=>$artikull['KODNJESIA1'],strtolower('KODNJESIA2')=>$artikull['KODNJESIA2'],strtolower('KOEFICENTARTIKULLI')=>$artikull['KOEFICENTARTIKULLI'],strtolower('MAGAZINA')=>$artikull['MAGAZINA'],strtolower('AKTIV')=>$artikull['AKTIV'],strtolower('KLASA')=>$artikull['KLASA'],strtolower('GRUPI')=>$artikull['GRUPI'],strtolower('NENGRUPI')=>$artikull['NENGRUPI'],strtolower('TVSHKODI')=>$artikull['TVSHKODI'],strtolower('VENDODHJEARTIKULLI')=>$artikull['VENDODHJEARTIKULLI'],strtolower('PERSHKRIMI')=>$artikull['PERSHKRIMI'],strtolower('IMAZH')=>$artikull['IMAZH'],strtolower('VLERATVSH')=>$artikull['VLERATVSH'],strtolower('KODIDOGANORARTIKULLI')=>$artikull['KODIDOGANORARTIKULLI'],strtolower('ARTREF')=>$artikull['ARTREF'],strtolower('KODIFIKIMARTIKULLI')=>$artikull['KODIFIKIMARTIKULLI'],strtolower('KODIFIKIMARTIKULLI2')=>$artikull['KODIFIKIMARTIKULLI2'],strtolower('DTMODIFIKIM')=>$artikull['DTMODIFIKIM'],strtolower('IDSTATUSDOK')=>$artikull['IDSTATUSDOK'],strtolower('PERPESHORE')=>$artikull['PERPESHORE'],strtolower('LLOJGARANCIA')=>$artikull['LLOJGARANCIA'],strtolower('GARANCIA')=>'0']);
 				
-			}
-		}
+		// 	}
+		// }
 		
 		
 	break;
