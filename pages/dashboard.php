1e7d0715dea56c8c306f1b4750b51bf439e0323d
USER1-PC\user <a.gjini@live.com>
2017-04-30 17:22:02 +0200

    Changes 55

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -89,8 +89,8 @@
           </div>
             <div class="progress" style="display: none;">
               <div class="progress-bar progress-bar-striped progress-bar-danger active" role="progressbar"
-              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:0%">
-                40%
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:5%">
+                5%
               </div>
             </div>
           
