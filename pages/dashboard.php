22cdcbfbe7d38d2c5e356d94227acee735777786
USER1-PC\user <a.gjini@live.com>
2017-04-30 13:34:28 +0200

    changes 16

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -142,7 +142,7 @@
         url: ip + "/magazinatpost",
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({"art":[{"MARRE":"1990-01-01","NRCHUNK":0,"NRSEL":0,"PERDORUES":"dea"}]}),
+        data: JSON.stringify({"art":[{"MARRE":"1990-01-01","NRCHUNK":0,"NRSEL":400,"PERDORUES":"dea"}]}),
         dataType: 'json',
         headers: {
         'ndermarrjaserver': 'MAXOPTIKA'
