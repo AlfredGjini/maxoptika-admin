112e29b2f0d90379ad94b05daeff46ae3a3682d1
USER1-PC\user <a.gjini@live.com>
2017-04-30 16:56:02 +0200

    Changes 48

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -86,11 +86,12 @@
               <i class="fa fa-database"></i>
             </div>
             <!-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
-          </div>
-          <div id="container" style="width:100%; height:50px; border:1px solid black;">
-            <div id="progress-bar" style="width:50%;background-color:#b0112b;height:48px;">
+            <div id="container" style="width:100%; height:30px; border:1px solid black;border-radius:5px;">
+              <div id="progress-bar" style="width:50%;background-color:#b0112b;height:28px;border-radius:5px;">
+              </div>
             </div>
           </div>
+          
         </div> 
         <!-- ./col -->
         <?php } ?>
