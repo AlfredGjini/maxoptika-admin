c2a8f7f7a907bcfacd42e92fe03edbba2ad6eb7e
USER1-PC\user <a.gjini@live.com>
2017-04-30 17:12:35 +0200

    changes 52

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -91,6 +91,13 @@
               <div id="progress-bar" style="width:50%;background-color:#b0112b;height:28px;border-radius:5px;">
               </div>
             </div>
+
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+                40%
+              </div>
+            </div>
           
         </div> 
         <!-- ./col -->
