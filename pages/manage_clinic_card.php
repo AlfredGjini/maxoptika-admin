f8900a0100fc27ed406e34a732ab23031ca6850b
USER1-PC\user <a.gjini@live.com>
2017-06-23 16:56:47 +0200

    m 59

--- pages/manage_clinic_card.php
+++ pages/manage_clinic_card.php
@@ -24,7 +24,7 @@
             <!-- form start -->
 
             
-              <div class="form-group">
+              <div class="form-group" style="margin-bottom: 95px;">
                   <div class="col-sm-12">
                   <div><img class="gifloader pull-left" style="display: none;padding-top: 30px;width: 30px;" src="assets/img/loader.gif"></div>
                     <center>
@@ -47,7 +47,7 @@
                   </div>
                 </div>
     <div class="container-fluid" ng-show="erdhiKartela" ng-repeat="info in kartela">
-      <button  type="button" class="btn btn-info btn-block btn-lg" data-toggle="collapse" data-target="#formaNr{{info.id}}">Vizita numer: {{$index+1}} Date: {{info.data_vizites}}</button>
+      <button  type="button" class="btn btn-info btn-block btn-lg" style="margin-bottom: 15px;" data-toggle="collapse" data-target="#formaNr{{info.id}}">Vizita numer: {{$index+1}} Date: {{info.data_vizites}}</button>
     <div id="formaNr{{info.id}}" aria-expanded="false" class="collapse" style="height: 0px;">
     <form id="create_clinic_card" action="functions.php?action=create_clinic_card" method="post" class="form-horizontal " >
       <div class="box-body" >
