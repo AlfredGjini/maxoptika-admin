5a37cba1ba7a597e555914bca0da3018e210ea04
USER1-PC\user <a.gjini@live.com>
2017-06-24 13:46:43 +0200

    m 81

--- pages/manage_clinic_card.php
+++ pages/manage_clinic_card.php
@@ -53,7 +53,7 @@
     <div class="container-fluid" ng-show="erdhiKartela" ng-repeat="info in kartela">
       <button  type="button" class="btn btn-info btn-block btn-lg" style="margin-bottom: 15px;" data-toggle="collapse" data-target="#formaNr{{info.id}}">Vizita numer: {{$index+1}} Date: {{info.data_vizites}}</button>
     <div id="formaNr{{info.id}}" aria-expanded="false" class="collapse" style="height: 0px;">
-    <form id="create_clinic_card" action="functions.php?action=create_clinic_card" method="post" class="form-horizontal " >
+    <form id="create_clinic_card"  method="post" class="form-horizontal " >
       <div class="box-body" >
 
 <!--       <div >
