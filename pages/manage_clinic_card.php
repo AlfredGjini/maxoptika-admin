ff4ed40ed81b93728c30280abdd2ec5d8c65fae1
USER1-PC\user <a.gjini@live.com>
2017-06-24 12:06:29 +0200

    m 72

--- pages/manage_clinic_card.php
+++ pages/manage_clinic_card.php
@@ -284,6 +284,9 @@
   <?php
   if (isset($_GET['id'])) { ?>
     <script type="text/javascript">
+var app = angular.module('myApp', []);
+      app.controller('myCtrl', function($scope, $http, $timeout) {
+
     jQuery('.gifloader').show();
 
            jQuery.ajax({
@@ -300,16 +303,16 @@
                  console.log(response);
                  if(response.exist==3){
                   swal({
-            title: 'Deshironi ta krijoni tani',
-            text: "Kartela nuk ekziston per kete klient",
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Po!'
-          }).then(function () {
-            window.location.href = "home.php?page=create_clinic_card&id="+response.id;
-          })
+                      title: 'Deshironi ta krijoni tani',
+                      text: "Kartela nuk ekziston per kete klient",
+                      type: 'warning',
+                      showCancelButton: true,
+                      confirmButtonColor: '#3085d6',
+                      cancelButtonColor: '#d33',
+                      confirmButtonText: 'Po!'
+                    }).then(function () {
+                      window.location.href = "home.php?page=create_clinic_card&id="+response.id;
+                    })
                  }else{
                  setFieldValsClinicCard(response);
                  }
@@ -323,6 +326,8 @@
 
 
           });
+
+           });
 
     </script>
 
