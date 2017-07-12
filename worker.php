25f8f8b205be5e44e84e1239ffc118a54578358b
USER1-PC\user <a.gjini@live.com>
2017-07-12 17:11:16 +0200

    New insertion system 2

--- worker.php
+++ worker.php
@@ -39,27 +39,19 @@ $params3 = Input::get('dhena3');
 				// 	$aktiv=0;
 				// }
 
+				$aktiv=1;
+
 
 				
 
 				$update_products = DB::getInstance()->insert('products2',[strtolower('KODARTIKULLI')=>$artikull['KODARTIKULLI'],strtolower('PERSHKRIMARTIKULLI')=>$artikull['PERSHKRIMARTIKULLI'],strtolower('PERSHKRIMIANGARTIKULLI')=>$artikull['PERSHKRIMIANGARTIKULLI'],strtolower('KODNJESIA1')=>$artikull['KODNJESIA1'],strtolower('KODNJESIA2')=>$artikull['KODNJESIA2'],strtolower('KOEFICENTARTIKULLI')=>$artikull['KOEFICENTARTIKULLI'],strtolower('MAGAZINA')=>$artikull['MAGAZINA'],strtolower('AKTIV')=>$aktiv,strtolower('KLASA')=>$artikull['KLASA'],strtolower('GRUPI')=>$artikull['GRUPI'],strtolower('NENGRUPI')=>$artikull['NENGRUPI'],strtolower('TVSHKODI')=>$artikull['TVSHKODI'],strtolower('VENDODHJEARTIKULLI')=>$artikull['VENDODHJEARTIKULLI'],strtolower('PERSHKRIMI')=>$artikull['PERSHKRIMI'],strtolower('IMAZH')=>$artikull['IMAZH'],strtolower('VLERATVSH')=>$artikull['VLERATVSH'],strtolower('KODIDOGANORARTIKULLI')=>$artikull['KODIDOGANORARTIKULLI'],strtolower('ARTREF')=>$artikull['ARTREF'],strtolower('KODIFIKIMARTIKULLI')=>$artikull['KODIFIKIMARTIKULLI'],strtolower('KODIFIKIMARTIKULLI2')=>$artikull['KODIFIKIMARTIKULLI2'],strtolower('DTMODIFIKIM')=>$artikull['DTMODIFIKIM'],strtolower('IDSTATUSDOK')=>$artikull['IDSTATUSDOK'],strtolower('PERPESHORE')=>1,strtolower('LLOJGARANCIA')=>$artikull['LLOJGARANCIA'],strtolower('GARANCIA')=>$artikull['GARANCIA'],strtolower('ORIGJINEARTIKULLI')=>$artikull['ORIGJINEARTIKULLI'],strtolower('SHASIA')=>$artikull['SHASIA'],strtolower('ZONAKADASTRALE')=>$artikull['ZONAKADASTRALE'],strtolower('MARKA')=>$artikull['MARKA'],strtolower('VITPRODHIMI')=>$artikull['VITPRODHIMI']]);
 				
-			// }else{
-			// 	echo "2";
-			// 	if ($artikull['AKTIV']==true) {
-			// 		$aktiv=1;
-			// 	}else{
-			// 		$aktiv=0;
-			// 	}
 
-
-			// 	$update_products = DB::getInstance()->insert('products2',[strtolower('KODARTIKULLI')=>$artikull['KODARTIKULLI'],strtolower('PERSHKRIMARTIKULLI')=>$artikull['PERSHKRIMARTIKULLI'],strtolower('PERSHKRIMIANGARTIKULLI')=>$artikull['PERSHKRIMIANGARTIKULLI'],strtolower('KODNJESIA1')=>$artikull['KODNJESIA1'],strtolower('KODNJESIA2')=>$artikull['KODNJESIA2'],strtolower('KOEFICENTARTIKULLI')=>$artikull['KOEFICENTARTIKULLI'],strtolower('MAGAZINA')=>$artikull['MAGAZINA'],strtolower('AKTIV')=>$aktiv,strtolower('KLASA')=>$artikull['KLASA'],strtolower('GRUPI')=>$artikull['GRUPI'],strtolower('NENGRUPI')=>$artikull['NENGRUPI'],strtolower('TVSHKODI')=>$artikull['TVSHKODI'],strtolower('VENDODHJEARTIKULLI')=>$artikull['VENDODHJEARTIKULLI'],strtolower('PERSHKRIMI')=>$artikull['PERSHKRIMI'],strtolower('IMAZH')=>$artikull['IMAZH'],strtolower('VLERATVSH')=>$artikull['VLERATVSH'],strtolower('KODIDOGANORARTIKULLI')=>$artikull['KODIDOGANORARTIKULLI'],strtolower('ARTREF')=>$artikull['ARTREF'],strtolower('KODIFIKIMARTIKULLI')=>$artikull['KODIFIKIMARTIKULLI'],strtolower('KODIFIKIMARTIKULLI2')=>$artikull['KODIFIKIMARTIKULLI2'],strtolower('DTMODIFIKIM')=>$artikull['DTMODIFIKIM'],strtolower('IDSTATUSDOK')=>$artikull['IDSTATUSDOK'],strtolower('PERPESHORE')=>1,strtolower('LLOJGARANCIA')=>$artikull['LLOJGARANCIA'],strtolower('GARANCIA')=>$artikull['GARANCIA'],strtolower('ORIGJINEARTIKULLI')=>$artikull['ORIGJINEARTIKULLI'],strtolower('SHASIA')=>$artikull['SHASIA'],strtolower('ZONAKADASTRALE')=>$artikull['ZONAKADASTRALE'],strtolower('MARKA')=>$artikull['MARKA'],strtolower('VITPRODHIMI')=>$artikull['VITPRODHIMI']]);
-				
-			// }
 		}
 		//echo "sukseseee2";
 
 		// Insert or update new cmime
+		$delete_cmimet = DB::getInstance()->deleteAll('cmime2');
 		foreach ($cmimet as $key => $cmimi) {
 			// $product_exist = DB::getInstance()->get('cmime',[['idprodukti','=',$gjendje['KODARTIKULLI']]])->firstResult();
 			//$artikull1=json_decode(json_encode($artikull), true);
@@ -68,26 +60,14 @@ $params3 = Input::get('dhena3');
 			$cmimi=(array) $cmimi;
 			//var_dump($artikull2);
 
-			$cmimi_exist = DB::getInstance()->get('cmime2',[['idprodukti','=',$cmimi['KODARTIKULLI']]])->firstResult();
-			//var_dump($product_exist);
-			
-			if($cmimi_exist!=0){
-				$delete_cmimi = DB::getInstance()->delete('cmime2',[['idprodukti','=',$cmimi['KODARTIKULLI']]]);
-
 
 				$update_products = DB::getInstance()->insert('cmime2',['idprodukti'=>$cmimi['KODARTIKULLI'],'cmimi'=>$cmimi['CMIMI'],'monedha'=>$cmimi['MONEDHAKOD']]);
 
-
-
-			}else{
-
-				$update_products = DB::getInstance()->insert('cmime2',['idprodukti'=>$cmimi['KODARTIKULLI'],'cmimi'=>$cmimi['CMIMI'],'monedha'=>$cmimi['MONEDHAKOD']]);
-
-			}
 		}
 
 
 		// Insert or update new magazina
+		$delete_cmimet = DB::getInstance()->deleteAll('magazina');
 		foreach ($magazinat as $key => $magazina) {
 			// $product_exist = DB::getInstance()->get('cmime',[['idprodukti','=',$gjendje['KODARTIKULLI']]])->firstResult();
 			//$artikull1=json_decode(json_encode($artikull), true);
@@ -95,23 +75,10 @@ $params3 = Input::get('dhena3');
 			//echo "hapsire";
 			$magazina=(array) $magazina;
 			//var_dump($artikull2);
-
-			$magazina_exist = DB::getInstance()->get('magazina',[['kodartikull','=',$magazina['KODARTIKULLI']]])->firstResult();
-			//var_dump($product_exist);
-			
-			if($magazina_exist!=0){
-				$delete_magazina = DB::getInstance()->delete('magazina',[['kodartikull','=',$magazina['KODARTIKULLI']]]);
-
+	
 
 				$update_products = DB::getInstance()->insert('magazina',['kodartikull'=>$magazina['KODARTIKULLI'],'sasia'=>$magazina['gjendje'],'njesia'=>$magazina['KODNJESIA1']]);
 
-
-
-			}else{
-
-				$update_products = DB::getInstance()->insert('magazina',['kodartikull'=>$magazina['KODARTIKULLI'],'sasia'=>$magazina['gjendje'],'njesia'=>$magazina['KODNJESIA1']]);
-
-			}
 		}
 
 
